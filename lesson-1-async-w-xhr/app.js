c80a6e96e6e11b4f684c3e4c2fa28014ab1fe2d4
Oscar Paesi <oscar.paesi@gmail.com>
2019-02-21 16:19:30 -0300

    Added loading of images and articles using XHR

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,51 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID b50d80c9704282139b456c159dede6de62843ec285273cc28d171861867d9c36');
+        unsplashRequest.send();
+
+        function addImage(){
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data && data.results && data.results[0]) {
+                const firstImage = data.results[0];
+                htmlContent = `<figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>"${firstImage.description}" by ${firstImage.user.name}</figcaption>
+                </figure>`;
+            } else {
+                htmlContent = '<div class="error-no-image">No images available</div>';
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=PWBQCT9yI5NgMunq93EA5dBGWeFqaYd0`);
+        articleRequest.send();
+
+        function addArticles() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data.response && data.response.docs && data.response.docs.length > 1) {
+                htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                        <p>${article.snippet}</p>
+                    </li>`
+                    ).join('') + '</ul>';
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
     });
+
 })();
